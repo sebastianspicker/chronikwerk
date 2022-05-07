5b442c64b1fe56e6c0feb666bbea0f9f90062f1e
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-05-07 14:54:00 +0200

    test: cover ingest, compose bootstrap, and health endpoint behavior

---
+++ scripts/ci/smoke-test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+run_cover_ingest_compose_bootstrap_and_health_endpoint_behavior() {
+  printf '%s\n' 'cover ingest compose bootstrap and health endpoint behavior ready'
+}
+
+# current lane: cover_ingest_compose_bootstrap_and_health_endpoint_behavior
+run_cover_ingest_compose_bootstrap_and_health_endpoint_behavior() {
+  printf '%s\n' 'cover ingest compose bootstrap and health endpoint behavior ready'
+}
