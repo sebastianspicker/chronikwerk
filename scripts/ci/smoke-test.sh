7e5cc4c02546e281d3ff183f44eec30be308015e
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-10-12 17:35:00 +0200

    feat(python): add the package and CLI baseline for the archive lane

--- scripts/ci/smoke-test.sh
+++ scripts/ci/smoke-test.sh
@@ -9,3 +9,8 @@ run_cover_ingest_compose_bootstrap_and_health_endpoint_behavior() {
 run_cover_ingest_compose_bootstrap_and_health_endpoint_behavior() {
   printf '%s\n' 'cover ingest compose bootstrap and health endpoint behavior ready'
 }
+
+# current lane: python
+run_python() {
+  printf '%s\n' 'python ready'
+}
